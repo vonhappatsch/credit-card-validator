f0483830d6125ff8a30578950857a17ce2a3d739
vonhappatsch <vonhappatsch@gmail.com>
2019-05-01 21:41:28 -0300

    adiciona eslint de acordo com guia de estilos e função exportada

---
+++ .eslintrc.js
@@ -0,0 +1,107 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018
+    },
+    "rules": {
+        "indent": [
+            "error",
+            2
+        ],
+        "linebreak-style": [
+            "error",
+            "windows"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "id-length": [
+          "error",
+          { "min": 2 }
+        ],
+        "camelcase": [
+          "error",
+          { "properties": "always" }
+        ],
+        "keyword-spacing": [
+          "error",
+          { "after": true }
+        ],
+        "space-infix-ops": [
+          "error", 
+          {"int32Hint": false}
+        ],
+        "comma-spacing": [
+          "error", 
+          { "before": false, "after": true }
+        ],
+        "space-before-blocks": [
+          "error",
+          "always"
+        ],
+        "spaced-comment": [
+          "error",
+          "always"
+        ],
+        "key-spacing": [
+          "error", 
+          { "afterColon": true }
+        ],
+        "no-multi-spaces": [
+          "error"
+        ],
+        "space-in-parens": [
+          "error", 
+          "never"
+        ],
+        "space-before-function-paren": [
+          "error", 
+          "never"
+        ],
+        "func-call-spacing": [
+          "error", 
+          "never"
+        ],
+        "no-multiple-empty-lines": [
+          "error", 
+          { "max": 0 }
+        ],
+        "allowSingleLineBlocks": [
+          "error",
+          "never"
+        ],
+        "quotes": [
+          "error", 
+          "single"
+        ],
+        "no-floating-decimal": [
+          "error"
+        ],
+        "object-property-newline": [
+          "error"
+        ],
+        "brace-style": [
+          "error"
+        ],
+        "eqeqeq": [
+          "error"
+        ],
+        "no-array-constructor": [
+          "error"
+        ],
+    }
+};
--- index.js
+++ index.js
@@ -4,55 +4,25 @@ const cardNumber = (card) => {
     .split('')
     .reverse()
     .map(Number);
-    console.log("value = " + value);
   return value;
-}
-
-const multiplyDigits = (value) => {
-  let multiDigits = value.map(function(curr, index) {
-    if (index % 2 === 0) {
-      curr = curr * 2;
-    } 
-    return curr;
-  });
-  console.log("multiDigits = " + multiDigits);
-  return multiDigits;
-}
-
-const overNine = (multiDigits) => {
-  let overNineDigits = multiDigits.map(function(curr) {
-    if (curr > 9) {
-      curr = curr - 9;
-    }
-    return curr;
-  });
-  console.log(overNineDigits);
-  return overNineDigits;
-}
-
-const sumMultipliedDigits = (overNineDigits) => {
-  let sum = overNineDigits.reduce((accum, curr) => accum += curr);
-  console.log(sum);
-  return sum;
-}
-
+};
+const sumMultDigits = (value) => {
+  let lastDigit = value.splice(0, 1)[0];
+  let sum = value.reduce((acc, curr, index) => (index % 2 !== 0 ? acc + curr : acc + ((curr * 2) % 9) || 9), 0);
+  sum += lastDigit;
+  return sum; 
+};
 const checkSum = (sum) => {
   if (sum % 10 === 0) {
     return true;
   } else {
     return false;
-  };
-}
-
+  }
+};
 function cardValidator(card) {
   const number = cardNumber(card);
-  const multiplying = multiplyDigits(number);
-  const multOverNine = overNine(multiplying);
-  const totalSum = sumMultipliedDigits(multOverNine);
+  const totalSum = sumMultDigits(number);
   const check = checkSum(totalSum);
   return check;
 }
-
-console.log(cardValidator(378282246310005));
-console.log(cardValidator(5555555555554444));
-console.log(cardValidator("4904182562658232"));
+module.exports.cardValidator = cardValidator;

453bf4144f1d60e8dc7aa144e4beb161c7e80a75
vonhappatsch <vonhappatsch@gmail.com>
2019-05-02 02:26:38 -0300

    função

--- index.js
+++ index.js
@@ -2,15 +2,24 @@ const cardNumber = (card) => {
   let value = card
     .toString()
     .split('')
-    .reverse()
-    .map(Number);
+    .reverse();
   return value;
 };
-const sumMultDigits = (value) => {
-  let lastDigit = value.splice(0, 1)[0];
-  let sum = value.reduce((acc, curr, index) => (index % 2 !== 0 ? acc + curr : acc + ((curr * 2) % 9) || 9), 0);
-  sum += lastDigit;
-  return sum; 
+const check = (value) => {
+  let sum = 0;
+  let curr = 0;
+  for (let index = value.length - 1; index < 0; index++) {
+    if (index % 2 === 0) {
+      curr = 2 * value[index];
+      if (curr > 9) {
+        curr = curr - 9;
+      }
+    } else {
+      curr = value[index];
+    }
+    sum += curr;
+  }
+  return sum;
 };
 const checkSum = (sum) => {
   if (sum % 10 === 0) {
@@ -21,8 +30,12 @@ const checkSum = (sum) => {
 };
 function cardValidator(card) {
   const number = cardNumber(card);
-  const totalSum = sumMultDigits(number);
-  const check = checkSum(totalSum);
-  return check;
+  const checkOne = check(number);
+  const finalCheck = checkSum(checkOne);
+  return finalCheck;
 }
 module.exports.cardValidator = cardValidator;
+
+console.log(cardValidator('5502881535322117'));
+console.log(cardValidator(5216572988775943));
+console.log(cardValidator(4929988486581206))
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,20 @@
+const multDigits = (value) => {
+//   let len = value.slice(-1);
+//   len = len.map(Number);
+//   console.log(len);
+//   let mult = len.map(function(curr, index) {
+//     if (index % 2 === 0) {
+//       curr = 2 * curr;
+//       if (curr > 9) {
+//         curr = curr - 9;
+//       }
+//     } else {
+//       curr;
+//     }
+//     let amount = 0;
+//     amount += curr;
+//     return amount;
+//   });
+//    console.log(mult);
+//    return mult;
+// };
